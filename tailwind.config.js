b036d7f4c2eecdba3dd23bb1ff5e4fc7821ed1a7
Samuel Gomez <gomezapari20@gmail.com>
2025-01-13 22:37:09 -0500

    add changes in styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,8 @@ module.exports = {
         'xs': '375px',
       },
       fontFamily: {
-        'ibm-plex-mono': ['"IBM Plex Mono"', 'serif'],
         'press-start-2p': ['"Press Start 2P"', 'serif'],
       },
-      colors: {
-        'primary': '#5D04D9',
-        'secondary': '#03318C',
-      }
     },
   },
   plugins: [],
